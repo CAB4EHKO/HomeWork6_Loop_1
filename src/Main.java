5b94980ac475469cc93d713c69c8e31d5852ca69
Savchenko <savchenko.ra@yandex.ru>
2023-02-20 12:53:47 +0300

    Task №7.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         task4();
         task5();
         task6();
+        task7();
     }
     public static void task1() {
         System.out.println("Task 1.");
@@ -42,8 +43,14 @@ public class Main {
         }
     }
     public static void task6() {
-        System.out.println("Task 6");
+        System.out.println("Task 6.");
         for (int i = 7; i <= 98; i+=7) {
+            System.out.println(i);
+        }
+    }
+    public static void task7() {
+        System.out.println("Task 7.");
+        for (int i = 1; i <= 512; i += i) {
             System.out.println(i);
         }
     }
